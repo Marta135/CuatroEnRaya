d3553f408a44ac6d70c5d155816a91fce19ddb42
marti <marti@192.168.1.37>
2021-01-15 17:17:19 +0100

    6. Creación del método estaOcupada, para la clase Casilla.

--- src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Casilla.java
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Casilla.java
@@ -25,10 +25,27 @@ public class Casilla {
 
 	public void setFicha(Ficha ficha) {
 		
-		if (ficha==null)
+		if (ficha==null) 
 			throw new NullPointerException("ERROR: No se puede poner una ficha nula.");
-		else
+		else 
 			this.ficha = ficha;
 	}
 	
+	
+	/********OTROS MÉTODOS********/
+	/**
+	 * Método estaOcupada
+	 * Devolverá true o false, dependiendo si la casilla está ocupada o no por
+	 * una ficha. 
+	 */
+	public boolean estaOcupada() {
+		
+		boolean casillaOcupada = false;
+		
+		if (ficha==Ficha.AZUL || ficha==Ficha.VERDE) {
+			casillaOcupada = true;
+		}
+			
+		return casillaOcupada;
+	}
 }
