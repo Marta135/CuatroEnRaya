1971d2749ab5fa26c5ba072b211cda08823fe056
marti <marti@192.168.1.37>
2021-01-15 14:30:12 +0100

    4. Creación de los métodos get y set para la clase Casilla.

--- src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Casilla.java
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Casilla.java
@@ -4,6 +4,20 @@ public class Casilla {
 
 	/*********ATRIBUTOS*********/
 	private Ficha ficha;
+
 	
+	/*********GETTERS Y SETTERS**********/
+	
+	public Ficha getFicha() {
+		return ficha;
+	}
+
+	public void setFicha(Ficha ficha) {
+		
+		if (ficha==null)
+			throw new NullPointerException("ERROR: No se puede poner una ficha nula.");
+		else
+			this.ficha = ficha;
+	}
 	
 }
