ed2fa9682e6537e86bb9928d28a2b3796b7eaa17
marti <marti@MSI>
2021-01-17 15:14:11 +0100

    13. Creación de la clase Tablero y sus atributos.

---
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Tablero.java
@@ -0,0 +1,13 @@
+package org.iesalandalus.programacion.cuatroenraya.modelo;
+
+public class Tablero {
+
+	/*********ATRIBUTOS*********/
+	private Casilla[][] casillas;
+	public static final int FILAS = 6;
+	public static final int COLUMNAS = 7;
+	public static final int FICHAS_IGUALES_CONSECUTIVAS_NECESARIAS = 4;
+	
+	
+	
+}
