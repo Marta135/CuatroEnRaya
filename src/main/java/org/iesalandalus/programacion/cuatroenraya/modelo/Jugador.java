4907c439de3cdc7edf85710cc4a1239860420b1b
marti <marti@MSI>
2021-01-17 14:32:52 +0100

    10. Creación de los métodos get y set de la clase Jugador.

--- src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Jugador.java
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Jugador.java
@@ -5,6 +5,39 @@ public class Jugador {
 	/*********ATRIBUTOS*********/
 	private String nombre;
 	private Ficha colorFichas;
+	
+		
+	/*********GETTERS Y SETTERS**********/
+	
+	public Ficha getColorFichas() {
+		return colorFichas;
+	}
+	
+	private void setColorFichas(Ficha colorFichas) {
+		
+		if (colorFichas == null) {
+			throw new NullPointerException("ERROR: El color de las fichas no puede ser nulo.");
+		} else {
+			this.colorFichas = colorFichas;
+		}
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	private void setNombre(String nombre) {
+		
+		if (nombre == null) {
+			throw new NullPointerException("ERROR: El nombre no puede ser nulo.");
+		} else if (nombre.matches("") || nombre.matches("\\s+")) {
+			throw new IllegalArgumentException("ERROR: El nombre no puede estar vacío.");
+		} else {
+			this.nombre = nombre;
+		}
+	}
+	
+	
 }
 
 
