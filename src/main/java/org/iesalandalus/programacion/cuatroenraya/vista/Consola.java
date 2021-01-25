45eee352735c193f34e7299b5bccfa7d71ce16ae
marti <marti@192.168.1.38>
2021-01-25 20:47:02 +0100

    31. Creación de la clase Consola y su constructor.

---
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/vista/Consola.java
@@ -0,0 +1,19 @@
+package org.iesalandalus.programacion.cuatroenraya.vista;
+
+public class Consola {
+
+
+	/*******CONSTRUCTORES*******/
+	/**
+	 * Constructor:
+	 * Evita que se puedan crear instancias de esta clase.
+	 */
+	private Consola () {
+		
+	}
+	
+	
+	
+	
+	
+}
