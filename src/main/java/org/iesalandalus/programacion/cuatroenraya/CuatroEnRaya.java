72e32eccfc7a80d1cbd636b037fa46ffadd89f53
marti <marti@192.168.1.38>
2021-01-26 00:02:23 +0100

    37. Creación de la clase CuatroEnRaya y sus atributos.

---
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/CuatroEnRaya.java
@@ -0,0 +1,17 @@
+package org.iesalandalus.programacion.cuatroenraya;
+
+import org.iesalandalus.programacion.cuatroenraya.modelo.Jugador;
+import org.iesalandalus.programacion.cuatroenraya.modelo.Tablero;
+
+public class CuatroEnRaya {
+
+	/*********ATRIBUTOS*********/
+	private static final int NUMERO_JUGADORES = 2;
+	
+	private Jugador[] jugadores;
+	private Tablero tablero;
+	
+	
+	
+	
+}
