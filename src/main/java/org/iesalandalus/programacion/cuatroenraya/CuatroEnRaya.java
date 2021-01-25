384799e89cc9381475a7b43fb950cbad03dfd6aa
marti <marti@192.168.1.38>
2021-01-26 00:11:59 +0100

    38. Creación del constructor con parámetros de la clase CuatroEnRaya.

--- src/main/java/org/iesalandalus/programacion/cuatroenraya/CuatroEnRaya.java
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/CuatroEnRaya.java
@@ -12,6 +12,23 @@ public class CuatroEnRaya {
 	private Tablero tablero;
 	
 	
+	/*******CONSTRUCTORES*******/
+	/**
+	 * Constructor con parámetros:
+	 * Pasaremos dos instancias del tipo Jugador, representando a cada uno de
+	 * los jugadores.
+	 */
+	public CuatroEnRaya(Jugador jugador1, Jugador jugador2) {
+		
+		if (jugador1 == null || jugador2 == null) {
+			throw new NullPointerException("ERROR: el jugador no puede ser nulo. ");
+		}
+		
+		jugadores = new Jugador[NUMERO_JUGADORES];
+		jugadores[0] = jugador1;
+		jugadores[1] = jugador2;
+		tablero = new Tablero();
+	}
 	
 	
 }
